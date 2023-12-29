280609bcb3a49903064b24cf13cbb084781311aa
Julia Crumenauer <julia.crumenauer@gmail.com>
2023-12-28 23:50:13 -0300

    botão editar

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    return res.render("index", {tasksList});
+    return res.render("index", {tasksList, task: null});
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -24,7 +24,14 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  const task = await Task.findOne({_id: req.params.id});
+  const tasksList = await Task.find();
+  res.render("index", {task, tasksList});
+}
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
 };
